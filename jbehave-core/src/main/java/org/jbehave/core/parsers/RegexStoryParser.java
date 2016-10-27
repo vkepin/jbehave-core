614951502118e48d3cdb47403cc1b9b13769ddaa
aliaksandrts <Aliaksandr_Tsymbal@epam.com>
2016-10-27 12:51:14 +0300

    Fix parsing of scenarios without steps

--- jbehave-core/src/main/java/org/jbehave/core/parsers/RegexStoryParser.java
+++ jbehave-core/src/main/java/org/jbehave/core/parsers/RegexStoryParser.java
@@ -285,7 +285,8 @@ public class RegexStoryParser implements StoryParser {
 
     private String findScenarioTitle(String scenarioAsText) {
         Matcher findingTitle = findingScenarioTitle().matcher(scenarioAsText);
-        return findingTitle.find() ? findingTitle.group(1).trim() : NONE;
+        return findingTitle.find() ? findingTitle.group(1).trim()
+                : StringUtils.substringAfter(scenarioAsText, keywords.scenario() + "\n").trim();
     }
 
     private Meta findScenarioMeta(String scenarioAsText) {
@@ -382,7 +383,7 @@ public class RegexStoryParser implements StoryParser {
 
     private Pattern findingScenarioMeta() {
         String startingWords = concatenateWithOr("\\n", "", keywords.startingWords());
-        return compile(".*" + keywords.meta() + "(.*?)\\s*(" + keywords.givenStories() + "|" + startingWords + ").*",
+        return compile(".*" + keywords.meta() + "(.*?)\\s*(" + keywords.givenStories() + "|" + startingWords + "|$).*",
                 DOTALL);
     }
 
