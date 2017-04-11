33d5a38cb3029a2a7bdcdc773d70a4d2b1da3973
Ivan Bachyla <ivbachilo@gmail.com>
2017-04-11 14:00:05 +0300

    Add possibility to specify variables to be extracted from story-level Examples tables

---
+++ jbehave-core/src/main/java/org/jbehave/core/parsers/ExamplesCutException.java
@@ -0,0 +1,12 @@
+package org.jbehave.core.parsers;
+
+public class ExamplesCutException extends IllegalArgumentException {
+
+    private static final long serialVersionUID = -3359090369403190356L;
+
+    public ExamplesCutException() {}
+
+    public ExamplesCutException(String s) {
+        super(s);
+    }
+}
--- jbehave-core/src/test/java/org/jbehave/core/embedder/StoryManagerBehaviour.java
+++ jbehave-core/src/test/java/org/jbehave/core/embedder/StoryManagerBehaviour.java
@@ -2,22 +2,34 @@ package org.jbehave.core.embedder;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyListOf;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 
 import org.codehaus.plexus.util.FileUtils;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.embedder.StoryManager.RunningStory;
+import org.jbehave.core.failures.BatchFailures;
+import org.jbehave.core.model.Story;
+import org.jbehave.core.parsers.ExamplesCutException;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class StoryManagerBehaviour {
+
+	private static final String STORY_PATH = "storyPath";
 
 	private PerformableTree performableTree = new PerformableTree();
 	private EmbedderMonitor embedderMonitor = new NullEmbedderMonitor(); 
@@ -38,4 +50,19 @@ public class StoryManagerBehaviour {
 		assertThat(outputDirectory.exists(), is(true));
 	}
 
+	@Test
+	public void shouldHandleExceptionAtStoryParsing() {
+		Configuration mockedConfiguration = mock(Configuration.class);
+		EmbedderMonitor mockedEmbedderMonitor = mock(EmbedderMonitor.class);
+		PerformableTree mockedPerformableTree = mock(PerformableTree.class);
+		StoryManager spy = Mockito.spy(new StoryManager(mockedConfiguration, stepsFactory, embedderControls, mockedEmbedderMonitor, executorService, mockedPerformableTree));
+		Exception expected = new ExamplesCutException();
+		when(mockedPerformableTree.storyOfPath(mockedConfiguration, STORY_PATH)).thenThrow(expected);
+		doNothing().when(spy).runStories(anyListOf(Story.class), any(MetaFilter.class), any(BatchFailures.class));
+		MetaFilter metaFilter = new MetaFilter();
+		BatchFailures batchFailures = new BatchFailures();
+		spy.runStoriesAsPaths(Collections.singletonList(STORY_PATH), metaFilter, batchFailures);
+		verify(mockedEmbedderMonitor).storyFailed(STORY_PATH, expected);
+		verify(spy).runStories(Collections.<Story>emptyList(), metaFilter, batchFailures);
+	}
 }
