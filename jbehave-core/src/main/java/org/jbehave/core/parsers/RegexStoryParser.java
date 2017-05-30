45bde24fe338923eeb671e9c60aec7cca78947dd
Ivan Bachyla <ivbachilo@gmail.com>
2017-05-30 12:52:30 +0300

    Update parametrized Examples to work on scenario-level

--- jbehave-core/src/main/java/org/jbehave/core/parsers/RegexStoryParser.java
+++ jbehave-core/src/main/java/org/jbehave/core/parsers/RegexStoryParser.java
@@ -39,7 +39,7 @@ public class RegexStoryParser implements StoryParser {
     private final Keywords keywords;
     private final ExamplesTableFactory tableFactory;
     private Pattern stepSkipPattern;
-    private Meta storySkipMeta;
+    private Meta skipMeta;
     private String skippedExample;
 
     public RegexStoryParser() {
@@ -64,26 +64,24 @@ public class RegexStoryParser implements StoryParser {
         Meta meta = parseStoryMetaFrom(storyAsText);
         Narrative narrative = parseNarrativeFrom(storyAsText);
         GivenStories givenStories = parseGivenStories(storyAsText);
-        Lifecycle lifecycle = null;
         try {
-            lifecycle = parseLifecycle(storyAsText);
+            Lifecycle lifecycle = parseLifecycle(storyAsText);
+            if (lifecycle == null) {
+                meta = meta.inheritFrom(skipMeta);
+                lifecycle = Lifecycle.EMPTY;
+            } else {
+                ExamplesTable storyExamplesTable = lifecycle.getExamplesTable();
+                if (!storyExamplesTable.isEmpty()) {
+                    useExamplesTableForGivenStories(givenStories, storyExamplesTable);
+                }
+            }
+            List<Scenario> scenarios = parseScenariosFrom(storyAsText);
+            Story story = new Story(storyPath, description, meta, narrative, givenStories, lifecycle, scenarios);
+            return nameStory(story, storyPath);
         } catch (ExamplesCutException ex) {
             return nameStory(new FailedStory(storyPath, meta, "Exception at story parsing", "Exception at building " +
                     "Examples Table", ex), storyPath);
         }
-        if (lifecycle == null) {
-            meta = meta.inheritFrom(storySkipMeta);
-            lifecycle = Lifecycle.EMPTY;
-        }
-        else {
-            ExamplesTable storyExamplesTable = lifecycle.getExamplesTable();
-            if (!storyExamplesTable.isEmpty()) {
-                useExamplesTableForGivenStories(givenStories, storyExamplesTable);
-            }
-        }
-        List<Scenario> scenarios = parseScenariosFrom(storyAsText);
-        Story story = new Story(storyPath, description, meta, narrative, givenStories, lifecycle, scenarios);
-        return nameStory(story, storyPath);
     }
 
     private Story nameStory(Story story, String storyPath) {
@@ -278,6 +276,10 @@ public class RegexStoryParser implements StoryParser {
         scenarioWithoutTitle = startingWithNL(scenarioWithoutTitle);
         Meta meta = findScenarioMeta(scenarioWithoutTitle);
         ExamplesTable examplesTable = findExamplesTable(scenarioWithoutTitle);
+        if(examplesTable == null) {
+            examplesTable = ExamplesTable.EMPTY;
+            meta = meta.inheritFrom(skipMeta);
+        }
         GivenStories givenStories = findScenarioGivenStories(scenarioWithoutTitle);
         useExamplesTableForGivenStories(givenStories, examplesTable);
         List<String> steps = findSteps(scenarioWithoutTitle);
@@ -356,7 +358,7 @@ public class RegexStoryParser implements StoryParser {
     private void checkTableAlignment(String skipKeyword, Map<String, String> examplesTableRow) {
         for (Map.Entry<String, String> entry : examplesTableRow.entrySet()) {
             if (!skipKeyword.equals(entry.getValue())) {
-                throw new ExamplesCutException("Story refers to variables with different number of examples at story level");
+                throw new ExamplesCutException("Story or scenario refer to variables with different number of examples");
             }
         }
     }
@@ -479,8 +481,8 @@ public class RegexStoryParser implements StoryParser {
         this.stepSkipPattern = stepSkipPattern;
     }
 
-    public void setStorySkipMeta(Meta storySkipMeta) {
-        this.storySkipMeta = storySkipMeta;
+    public void setSkipMeta(Meta skipMeta) {
+        this.skipMeta = skipMeta;
     }
 
     public void setSkippedExample(String skippedExample) {
